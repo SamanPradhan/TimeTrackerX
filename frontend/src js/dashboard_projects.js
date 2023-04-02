bc43b845d73bfbfd71136200a139a17837114050
Saman <pradhansaman2017@gmail.com>
2023-04-02 09:05:21 +0530

    front end work schedules

--- frontend/src js/dashboard_projects.js
+++ frontend/src js/dashboard_projects.js
@@ -71,3 +71,90 @@ async function createProject() {
     })
     .catch((err) => console.log(err));
 }
+
+//getting new Product
+
+let body = document.getElementById("tbody");
+showProducts();
+function showProducts() {
+  fetch("http://localhost:4500/projects/", {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: `${localStorage.getItem("token")}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      display(res);
+    })
+    .catch((err) => console.log(err));
+}
+
+function display(data) {
+  body.innerHTML = "";
+
+  data.forEach((element) => {
+    let tr = document.createElement("tr");
+    let pname = document.createElement("td");
+    let perhrcharge = document.createElement("td");
+    let estimatecosting = document.createElement("td");
+    let estimateTimetake = document.createElement("td");
+    let noteIdprint = document.createElement("td");
+    let editBtn = document.createElement("td");
+    let deleteBtn = document.createElement("td");
+    // deleteBtn.addEventListener("click", () => {
+    //   data = data.filter((ele) => {
+    //     console.log(element);
+    //     return ele.id !== element.id;
+    //   });
+    //   // localStorage.setItem("data", JSON.stringify(CartArr));
+    //   display(data);
+    //   alert("The Project is deleted");
+    // });
+    pname.innerText = element.projectName;
+    perhrcharge.innerText = element.perHourCharge;
+    estimatecosting.innerText = element.estimateCost;
+    estimateTimetake.innerText = element.estimateTime;
+    noteIdprint.innerText = element._id;
+    editBtn.innerText = "edit";
+    deleteBtn.innerText = "delete";
+    deleteBtn.setAttribute("id", "deleteBtn");
+    tr.append(
+      pname,
+      perhrcharge,
+      estimatecosting,
+      estimateTimetake,
+      noteIdprint,
+      editBtn,
+      deleteBtn
+    );
+    body.append(tr);
+  });
+}
+
+let deleteBtn = document.getElementById("deleteBtn");
+
+deleteBtn.addEventListener("click", () => {
+  deleteNotes();
+
+  function deleteNotes() {
+    // let noteId = noteid.value;
+    // console.log(noteId);
+    console.log();
+    fetch(`http://localhost:4500/projects/delete/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        alert(JSON.stringify(res.msg));
+      })
+      .catch((err) => console.log(err));
+  }
+});
